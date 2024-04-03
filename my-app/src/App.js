785afa39c1d54fab527e478631883f4fd1ac977c
FelineJTD <13520050@std.stei.itb.ac.id>
2024-04-03 15:23:48 +0700

    feat: font

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,8 @@
 import { useEffect, useRef, useState } from "react";
-import logo from './logo.svg';
-import './App.css';
+import './styles/text.css';
 import mqtt from "precompiled-mqtt";
+import Transaction from "./components/transaction";
+import Clock from "./components/clock";
 
 function App() {
   // Params
@@ -11,19 +12,25 @@ function App() {
 
   const mqttUrl = `mqtts://${mqttHost}:${mqttPort}`;
   const client = useRef(null);
-  const status = useState("Disconnected");
+  const [status, setStatus] = useState("Disconnected");
+  const [transactions, setTransactions] = useState([]);
 
   useEffect(() => {
     client.current = mqtt.connect(mqttUrl);
     client.current.on("connect", () => {
       console.log("connected");
+      setStatus("Connected");
       client.current.subscribe(mqttTopic);
     });
     client.current.on("message", (topic, message) => {
       console.log(topic, message.toString());
+      setTransactions((prevTransactions) => {
+        return [...prevTransactions, message];
+      });
     });
     client.current.on("reconnect", () => {
       console.log("reconnect");
+      setStatus("Reconnecting...");
     });
 
     return () => {
@@ -33,8 +40,25 @@ function App() {
   , []);
 
   return (
-    <main className="bg-zinc-800 min-h-screen w-full">
-      <h1 className="">Merchant-UI</h1>
+    <main className="bg-zinc-800 text-zinc-50 min-h-screen w-full py-24 px-24 xl:px-[20vw]">
+      {/* HEADER */}
+      <header className="flex justify-between">
+        <h1 className="">Merchant-UI</h1>
+        <div className="text-right">
+          <Clock />
+          <p className={status != "Connected" ? "text-red-500" : "text-green-500"}><b>● {status}</b></p>
+        </div>
+      </header>
+      {/* INCOMING TRANSACTION/STATUS WINDOW */}
+      <div>
+
+      </div>
+      {/* TRANSACTION HISTORY */}
+      <div>
+        {transactions.map((transaction, index) => {
+          <Transaction key={index} transaction={transaction} />
+        })}
+      </div>
     </main>
   );
 }
--- my-app/src/components/transaction.js
+++ my-app/src/components/transaction.js
@@ -1,52 +1,8 @@
-import { useEffect, useRef } from "react";
-import logo from './logo.svg';
-import './App.css';
-import mqtt from "precompiled-mqtt";
-
 function Transaction({ transaction }) {
-  // Params
-  const mqttHost = "test.mosquitto.org";
-  const mqttPort = 8081;
-  const mqttTopic = "13520050-if4051-out";
-
-  const mqttUrl = `mqtts://${mqttHost}:${mqttPort}`;
-  const client = useRef(null);
-
-  useEffect(() => {
-    client.current = mqtt.connect(mqttUrl);
-    client.current.on("connect", () => {
-      console.log("connected");
-      client.current.subscribe(mqttTopic);
-    });
-    client.current.on("message", (topic, message) => {
-      console.log(topic, message.toString());
-    });
-    client.current.on("reconnect", () => {
-      console.log("reconnect");
-    });
-
-    return () => {
-      client.current.end();
-    };
-  }
-  , []);
-
+  console.log(transaction);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full text-zinc-50">
+      <p>{transaction}</p>
     </div>
   );
 }
