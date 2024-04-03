a701a83775b768d34b9962bac5235511f05ffa97
FelineJTD <13520050@std.stei.itb.ac.id>
2024-04-03 13:57:45 +0700

    feat: mqtt working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,28 @@
+import { useEffect, useRef } from "react";
 import logo from './logo.svg';
 import './App.css';
-import { Connector, subscribe } from "react-mqtt-client";
+import mqtt from "precompiled-mqtt";
 // import Connector from "react-mqtt-client/dist/Connector";
 
 function App() {
+  const client = useRef(null);
+
+  useEffect(() => {
+    client.current = mqtt.connect("mqtts://test.mosquitto.org:8081");
+    client.current.on("connect", () => {
+      console.log("connected");
+      client.current.subscribe("13520050-if4051-out");
+    });
+    client.current.on("message", (topic, message) => {
+      console.log(topic, message.toString());
+    });
+
+    return () => {
+      client.current.end();
+    };
+  }
+  , []);
+
   return (
     <div className="App">
       <header className="App-header">
