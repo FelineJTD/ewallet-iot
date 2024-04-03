25ef6379a8bc37d7f12f1384fae317345a82ed9a
FelineJTD <13520050@std.stei.itb.ac.id>
2024-04-03 19:06:55 +0700

    refactor: delete unused codes and rename folder

--- merchant-ui/src/App.js
+++ merchant-ui/src/App.js
@@ -26,7 +26,8 @@ function App() {
     });
     client.current.on("message", (topic, message) => {
       console.log(topic, message.toString());
-      setCurrTransaction(message.toString());
+      const parsed = message.toString().split(";");
+      setCurrTransaction(parsed);
     });
     client.current.on("reconnect", () => {
       console.log("reconnect");
@@ -37,7 +38,7 @@ function App() {
       client.current.end();
     };
   }
-  , []);
+  , [mqttUrl, mqttTopic]);
 
   useEffect(() => {
     if (!currTransaction) return;
@@ -58,24 +59,33 @@ function App() {
         <h1 className="">Merchant-UI</h1>
         <div className="text-right">
           <Clock />
-          <p className={status != "Connected" ? "text-red-500" : "text-green-500"}><b>● {status}</b></p>
+          <p className={status !== "Connected" ? "text-red-500" : "text-green-500"}><b>● {status}</b></p>
         </div>
       </header>
       {/* INCOMING TRANSACTION/STATUS WINDOW */}
       <div className="w-full h-48 border border-zinc-600 mt-8">
         { currTransaction ? 
-          <p>{currTransaction}</p> :
-          <AsciiAnimation />
+          <div className="w-full h-full py-8 px-16">
+            {/* status */}
+            <p>{currTransaction[1] === "failed" ? "PEMBAYARAN GAGAL." : "PEMBAYARAN SEBESAR " + currTransaction[2]  + " BERHASIL."}</p>
+            {/* saldo */}
+            <p>Sisa saldo Anda: {currTransaction[0]}</p>
+          </div>
+        :
+          <div className="w-full h-full flex flex-col items-center justify-center">
+            <AsciiAnimation />
+            <p className="text-center text-zinc-400">&nbsp;No ongoing transaction.</p>
+          </div>
         }
       </div>
       {/* TRANSACTION HISTORY */}
-      <div className="text-white flex flex-col w-full h-full">
+      <div className="text-white w-full pt-12">
         <h2>Transaction History</h2>
-        {/* <div className="flex flex-col w-full h-full"> */}
+        <div className="flex flex-col w-full gap-4 mt-6">
           {transactions.map((transaction, index) => (
             <Transaction key={index} transaction={transaction} />
           ))}
-        {/* </div> */}
+        </div>
       </div>
     </main>
   );
